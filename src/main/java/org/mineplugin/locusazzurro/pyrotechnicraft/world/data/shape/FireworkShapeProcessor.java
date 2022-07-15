4141b1ef374703df0cec1f3d303bf564f7453c13
LocusAzzurro <dingzx1997@gmail.com>
2022-07-15 12:40:59 +0200

    :art: extract tag name strings as constants

--- src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/data/shape/FireworkShapeProcessor.java
+++ src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/data/shape/FireworkShapeProcessor.java
@@ -7,14 +7,26 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 public class FireworkShapeProcessor {
+
+    private static final float FORCE_DEFAULT = 0.2f;
+    private static final float JITTER_DEFAULT = 0.1f;
+    public static final String SHAPE_DATA = "ShapeData";
+    public static final String FORCE = "Force";
+    public static final String SPARKS = "Sparks";
+    public static final String JITTER = "Jitter";
+    public static final String UNIFORM = "Uniform";
+    public static final String ROTATION_JITTER = "RotationJitter";
+    public static final String WIDENESS = "Wideness";
+    public static final String ABSOLUTE_ROTATION = "AbsoluteRotation";
+    public static final String ROTATIONS_LIST = "Rotations";
+    public static final String ROTATION_SINGLE = "Rotation";
+    public static final String COORDINATES = "Coordinates";
 
     static Map<ExplosionShape, BiFunction<CompoundTag, Vec3, ? extends IExplosionShape>> MAP = new HashMap<>();
 
@@ -22,25 +34,24 @@ public class FireworkShapeProcessor {
         ExplosionShape shape = ExplosionShape.MAP.getOrDefault(tag.getString("Shape"), ExplosionShape.SPHERE);
         return MAP.getOrDefault(shape, parseSphereExplosion).apply(tag, vec);
     }
-
     static BiFunction<CompoundTag, Vec3, SphereExplosion> parseSphereExplosion = (tag, vec) -> {
-        CompoundTag data = tag.getCompound("ShapeData");
-        float size = tag.contains("Force") ? tag.getFloat("Force") : 0.2f;
-        int sparks = tag.contains("Sparks") ? tag.getInt("Sparks") : ExplosionShape.SPHERE.getFireworkMixtureValue();
-        float jitter = data.contains("Jitter") ? data.getFloat("Jitter") : 0.1f;
-        boolean uniform = data.contains("Uniform") && data.getBoolean("Uniform");
+        CompoundTag data = tag.getCompound(SHAPE_DATA);
+        float size = tag.contains(FORCE) ? tag.getFloat(FORCE) : FORCE_DEFAULT;
+        int sparks = tag.contains(SPARKS) ? tag.getInt(SPARKS) : ExplosionShape.SPHERE.getFireworkMixtureValue();
+        float jitter = data.contains(JITTER) ? data.getFloat(JITTER) : JITTER_DEFAULT;
+        boolean uniform = data.contains(UNIFORM) && data.getBoolean(UNIFORM);
         return new SphereExplosion(size, sparks, jitter, uniform);
     };
 
     static BiFunction<CompoundTag, Vec3, RingExplosion> parseCircleExplosion = (tag, vec) -> {
-        CompoundTag data = tag.getCompound("ShapeData");
-        float size = tag.contains("Force") ? tag.getFloat("Force") : 0.2f;
-        int sparks = tag.contains("Sparks") ? tag.getInt("Sparks") : ExplosionShape.RING.getFireworkMixtureValue();
-        float jitter = data.contains("Jitter") ? data.getFloat("Jitter") : 0.1f;
-        float rotationJitter = data.contains("RotationJitter") ? data.getFloat("RotationJitter") : 0.1f;
-        boolean uniform = data.contains("Uniform") && data.getBoolean("Uniform");
-        boolean absolute = data.contains("AbsoluteRotation") && data.getBoolean("AbsoluteRotation");
-        ListTag rotationsList = data.contains("Rotations") ? data.getList("Rotations", 5) : null;
+        CompoundTag data = tag.getCompound(SHAPE_DATA);
+        float size = tag.contains(FORCE) ? tag.getFloat(FORCE) : FORCE_DEFAULT;
+        int sparks = tag.contains(SPARKS) ? tag.getInt(SPARKS) : ExplosionShape.RING.getFireworkMixtureValue();
+        float jitter = data.contains(JITTER) ? data.getFloat(JITTER) : JITTER_DEFAULT;
+        float rotationJitter = data.contains(ROTATION_JITTER) ? data.getFloat(ROTATION_JITTER) : 0.1f;
+        boolean uniform = data.contains(UNIFORM) && data.getBoolean(UNIFORM);
+        boolean absolute = data.contains(ABSOLUTE_ROTATION) && data.getBoolean(ABSOLUTE_ROTATION);
+        ListTag rotationsList = data.contains(ROTATIONS_LIST) ? data.getList(ROTATIONS_LIST, ListTag.TAG_FLOAT) : null;
         float[] rotations;
         if (rotationsList == null) rotations = new float[]{0, 0};
         else {
@@ -53,14 +64,14 @@ public class FireworkShapeProcessor {
     };
 
     static BiFunction<CompoundTag, Vec3, BurstExplosion> parseBurstExplosion = (tag, vec) -> {
-        CompoundTag data = tag.getCompound("ShapeData");
-        float size = tag.contains("Force") ? tag.getFloat("Force") : 0.2f;
-        int sparks = tag.contains("Sparks") ? tag.getInt("Sparks") : ExplosionShape.BURST.getFireworkMixtureValue();
-        float jitter = data.contains("Jitter") ? data.getFloat("Jitter") : 0.1f;
-        float rotationJitter = data.contains("RotationJitter") ? data.getFloat("RotationJitter") : 0.1f;
-        float wideness = data.contains("Wideness") ? data.getFloat("Wideness") : Mth.PI / 12;
-        boolean absolute = data.contains("AbsoluteRotation") && data.getBoolean("AbsoluteRotation");
-        ListTag rotationsList = data.contains("Rotations") ? data.getList("Rotations", 5) : null;
+        CompoundTag data = tag.getCompound(SHAPE_DATA);
+        float size = tag.contains(FORCE) ? tag.getFloat(FORCE) : FORCE_DEFAULT;
+        int sparks = tag.contains(SPARKS) ? tag.getInt(SPARKS) : ExplosionShape.BURST.getFireworkMixtureValue();
+        float jitter = data.contains(JITTER) ? data.getFloat(JITTER) : JITTER_DEFAULT;
+        float rotationJitter = data.contains(ROTATION_JITTER) ? data.getFloat(ROTATION_JITTER) : 0.1f;
+        float wideness = data.contains(WIDENESS) ? data.getFloat(WIDENESS) : Mth.PI / 12;
+        boolean absolute = data.contains(ABSOLUTE_ROTATION) && data.getBoolean(ABSOLUTE_ROTATION);
+        ListTag rotationsList = data.contains(ROTATIONS_LIST) ? data.getList(ROTATIONS_LIST, ListTag.TAG_FLOAT) : null;
         float[] rotations;
         if (rotationsList == null) rotations = new float[]{0, 0};
         else {
@@ -73,17 +84,17 @@ public class FireworkShapeProcessor {
     };
 
     static BiFunction<CompoundTag, Vec3, PlaneExplosion> parsePlaneExplosion = (tag, vec) -> {
-        CompoundTag data = tag.getCompound("ShapeData");
-        float size = tag.contains("Force") ? tag.getFloat("Force") : 0.2f;
-        float rotationJitter = data.contains("RotationJitter") ? data.getFloat("RotationJitter") : Mth.TWO_PI;
-        float rotation = data.contains("Rotation") ? data.getFloat("Rotation") : 0.0f;
-        boolean absolute = data.contains("AbsoluteRotation") && data.getBoolean("AbsoluteRotation");
+        CompoundTag data = tag.getCompound(SHAPE_DATA);
+        float size = tag.contains(FORCE) ? tag.getFloat(FORCE) : FORCE_DEFAULT;
+        float rotationJitter = data.contains(ROTATION_JITTER) ? data.getFloat(ROTATION_JITTER) : Mth.TWO_PI;
+        float rotation = data.contains(ROTATION_SINGLE) ? data.getFloat(ROTATION_SINGLE) : 0.0f;
+        boolean absolute = data.contains(ABSOLUTE_ROTATION) && data.getBoolean(ABSOLUTE_ROTATION);
         ListTag coordList = defaultPlaneCoords();
-        if (data.contains("Coordinates")){
-            ListTag coords = data.getList("Coordinates", 9);
+        if (data.contains(COORDINATES)){
+            ListTag coords = data.getList(COORDINATES, ListTag.TAG_LIST);
             if (!coords.isEmpty()) coordList = coords;
         }
-        List<Vec2> coordVecList = coordList.stream().filter(coord -> (coord instanceof ListTag coordPair && coordPair.getElementType() == 5))
+        List<Vec2> coordVecList = coordList.stream().filter(coord -> (coord instanceof ListTag coordPair && coordPair.getElementType() == ListTag.TAG_FLOAT))
                 .map(coord -> new Vec2(((ListTag) coord).getFloat(0), ((ListTag) coord).getFloat(1))).toList();
         if (coordVecList.isEmpty()) {
             coordVecList = defaultPlaneCoords().stream().map(coord -> new Vec2(((ListTag) coord).getFloat(0), ((ListTag) coord).getFloat(1))).toList();
