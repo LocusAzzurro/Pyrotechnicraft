2a4671a969f3c481ccb20de4a9e2b5a6e4da8245
LocusAzzurro <dingzx1997@gmail.com>
2022-07-19 11:44:31 +0200

    :bento: add texture for fuses, homing module and wrapping paper

--- src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/datagen/ModItemModelProvider.java
+++ src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/datagen/ModItemModelProvider.java
@@ -1,6 +1,7 @@
 package org.mineplugin.locusazzurro.pyrotechnicraft.datagen;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelBuilder;
@@ -16,26 +17,26 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        singleTexture(ItemRegistry.SULFUR.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/sulfur"));
-        singleTexture(ItemRegistry.SALTPETER.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/saltpeter"));
-        singleTexture(ItemRegistry.COAL_DUST.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/coal_dust"));
-        singleTexture(ItemRegistry.FIREWORK_MIXTURE.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/firework_mixture"));
-        singleTexture(ItemRegistry.FIREWORK_ORB_CORE.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/firework_orb_core"));
-        singleTexture(ItemRegistry.SPHERE_EXPLOSION_PATTERN.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/sphere_explosion_pattern"));
-        singleTexture(ItemRegistry.RING_EXPLOSION_PATTERN.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/ring_explosion_pattern"));
-        singleTexture(ItemRegistry.BURST_EXPLOSION_PATTERN.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/burst_explosion_pattern"));
-        singleTexture(ItemRegistry.PLANE_EXPLOSION_PATTERN.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/2d_explosion_pattern"));
-        singleTexture(ItemRegistry.MATRIX_EXPLOSION_PATTERN.get().getRegistryName().getPath(),
-                mcLoc("item/generated"), "layer0", modLoc("item/3d_explosion_pattern"));
+        generatedItem(ItemRegistry.SULFUR.get());
+        generatedItem(ItemRegistry.SALTPETER.get());
+        generatedItem(ItemRegistry.COAL_DUST.get());
+        generatedItem(ItemRegistry.FIREWORK_MIXTURE.get());
+        generatedItem(ItemRegistry.FIREWORK_ORB_CORE.get());
+        generatedItem(ItemRegistry.SPHERE_EXPLOSION_PATTERN.get());
+        generatedItem(ItemRegistry.RING_EXPLOSION_PATTERN.get());
+        generatedItem(ItemRegistry.BURST_EXPLOSION_PATTERN.get());
+        generatedItem(ItemRegistry.PLANE_EXPLOSION_PATTERN.get());
+        generatedItem(ItemRegistry.MATRIX_EXPLOSION_PATTERN.get());
+        generatedItem(ItemRegistry.INSTANT_FUSE.get());
+        generatedItem(ItemRegistry.REGULAR_FUSE.get());
+        generatedItem(ItemRegistry.EXTENDED_FUSE.get());
+        generatedItem(ItemRegistry.CUSTOM_FUSE.get());
+        generatedItem(ItemRegistry.FIREWORK_HOMING_MODULE.get());
+        generatedItem(ItemRegistry.FIREWORK_WRAPPING_PAPER.get());
+    }
+
+    private ItemModelBuilder generatedItem(Item item){
+        return singleTexture(item.getRegistryName().getPath(), mcLoc("item/generated"), "layer0", modLoc("item/" + item.getRegistryName().getPath()));
     }
 
     @NotNull
