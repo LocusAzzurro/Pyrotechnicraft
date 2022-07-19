b74fef5d8285b62814d31b90200bf87723f86574
LocusAzzurro <dingzx1997@gmail.com>
2022-07-19 18:34:15 +0200

    :sparkles: add facing block state to firework crafting tables

--- src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/block/AbstractFireworkCraftingTable.java
+++ src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/block/AbstractFireworkCraftingTable.java
@@ -1,33 +1,53 @@
 package org.mineplugin.locusazzurro.pyrotechnicraft.world.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Containers;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.items.CapabilityItemHandler;
-import org.mineplugin.locusazzurro.pyrotechnicraft.world.block.entity.FireworkOrbCraftingTableBlockEntity;
 
 public abstract class AbstractFireworkCraftingTable extends Block implements EntityBlock {
 
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public AbstractFireworkCraftingTable() {
         super(Properties.of(Material.WOOD).sound(SoundType.WOOD).strength(1.5f));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
         if (!pState.is(pNewState.getBlock())) {
             BlockEntity blockentity = pLevel.getBlockEntity(pPos);
-            blockentity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
-                for (int i = 0; i < handler.getSlots(); ++i) {
-                    Containers.dropItemStack(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(), handler.getStackInSlot(i));
-                }
-            });
+            if (blockentity != null) {
+                blockentity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
+                    for (int i = 0; i < handler.getSlots(); ++i) {
+                        Containers.dropItemStack(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(), handler.getStackInSlot(i));
+                    }
+                });
+            }
             super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
         }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+    @SuppressWarnings("deprecation")
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
     }
 }
