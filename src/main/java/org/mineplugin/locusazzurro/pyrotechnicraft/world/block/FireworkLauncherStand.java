201b5b7b67caf0059e7135386431b66d563b3735
LocusAzzurro <dingzx1997@gmail.com>
2022-07-22 12:03:11 +0200

    :bug: fix launcher stand not having model and block state

--- src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/block/FireworkLauncherStand.java
+++ src/main/java/org/mineplugin/locusazzurro/pyrotechnicraft/world/block/FireworkLauncherStand.java
@@ -1,13 +1,20 @@
 package org.mineplugin.locusazzurro.pyrotechnicraft.world.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.gameevent.GameEventListener;
 import net.minecraft.world.level.material.Material;
 import org.jetbrains.annotations.Nullable;
@@ -15,8 +22,10 @@ import org.mineplugin.locusazzurro.pyrotechnicraft.world.block.entity.FireworkLa
 
 public class FireworkLauncherStand extends BaseEntityBlock {
 
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public FireworkLauncherStand() {
         super(BlockBehaviour.Properties.of(Material.STONE));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Nullable
@@ -35,5 +44,19 @@ public class FireworkLauncherStand extends BaseEntityBlock {
     @Override
     public <T extends BlockEntity> GameEventListener getListener(Level pLevel, T pBlockEntity) {
         return super.getListener(pLevel, pBlockEntity);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
     }
 }
